e878a569d70fd62e70910cf0167dc129e32a6aba
zjeffer <vanhouttetuur@gmail.com>
2018-12-10 22:26:37 +0100

    fixed arch updates

--- polybar/scripts/arch-updates/updates-arch-combined.sh
+++ polybar/scripts/arch-updates/updates-arch-combined.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+if ! updates_arch=$(checkupdates 2>/dev/null | wc -l ); then
     updates_arch=0
 fi
 
-if ! updates_aur=$(yay -Qum | wc -l); then
+if ! updates_aur=$(yay -Qum 2>/dev/null | wc -l); then
     updates_aur=0
 fi
 
