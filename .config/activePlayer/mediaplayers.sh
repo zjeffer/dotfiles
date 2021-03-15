ff20213b92f06dfd191570effc5773e2d60b28c0
zjeffer <vanhouttetuur@gmail.com>
2021-03-15 19:11:16 +0100

    fixed active player script

--- .config/activePlayer/mediaplayers.sh
+++ .config/activePlayer/mediaplayers.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 
+get_players(){
+    echo `qdbus | egrep -i 'org.mpris.MediaPlayer2|plasma.browser_integration'`
+}
+
+get_status(){
+    echo `qdbus $1 /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus`
+}
+
 while true ; do
-	for m in `qdbus | egrep -i 'org.mpris.MediaPlayer2|plasma.browser_integration'` ; do
-		if [[ `qdbus $m /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus` == 'Playing' ]]; then
-			echo $m > ~/.config/activePlayer/currentPlaying.txt
-			break
+    for player in $(get_players) ; do
+        if [[ $(get_status $player) == 'Playing' ]]; then
+			echo $player > ~/.config/activePlayer/currentPlaying.txt
 			bool=0
-		elif [[ `qdbus $m /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus` == 'Stopped' ]]; then
+            break
+        elif [[ $(get_status $player) == 'Stopped' ]]; then
+            # if a player is stopped, we don't want to start it again
+            # often if i close firefox, the player will be marked as stopped while it should actually be deleted
 			bool=1
 		fi
-	done
-	#if [[ $bool == 1 ]]; then
-	#	for m in `qdbus | egrep -i 'org.mpris.MediaPlayer2|plasma.browser_integration'` ; do
-	#		if [[ `qdbus $m /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlaybackStatus` == 'Paused' ]]; then
-	#			echo $m > ~/.config/activePlayer/currentPlaying.txt
-	#			bool=0
-	#			break
-	#		fi
-	#	done
-	#fi
-	sleep 0.5
+    done
+    # if a player is stopped, search for the next paused player
+	if [[ $bool == 1 ]]; then
+        for player in $(get_players) ; do
+            if [[ $(get_status $player) == 'Paused' ]]; then
+				echo "$player" > ~/.config/activePlayer/currentPlaying.txt
+				bool=0
+				break
+			fi
+		done
+	fi
+	sleep 0.25
 done
