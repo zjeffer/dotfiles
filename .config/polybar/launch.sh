d6b09b606a140bd8f367ec674853fc001d4b568d
zjeffer <vanhouttetuur@gmail.com>
2021-07-07 21:06:40 +0200

    new laptop: legion 5 pro

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,7 +13,7 @@ done
 #Launch Polybar on every monitor, using default config location ~/.config/polybar/config
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	# put a different bar on the laptop monitor (because it has a higher resolution)
-	if [ "$m" = "eDP-1-2" ]; then
+	if [ "$m" = "DP-4" ]; then
 		MONITOR=$m polybar -r laptop 2>>~/.config/polybar/.polybar.err &
 	else
 		MONITOR=$m polybar -r custom 2>>~/.config/polybar/.polybar.err &
