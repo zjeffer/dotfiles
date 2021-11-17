3959edf45942329d3452338b8f755c1a7f6aa47b
zjeffer <4633209+zjeffer@users.noreply.github.com>
2021-11-17 20:32:19 +0100

    improved monitor configs in bspwm and polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -13,7 +13,7 @@ done
 #Launch Polybar on every monitor, using default config location ~/.config/polybar/config
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	# put a different bar on the laptop monitor (because it has a higher resolution)
-	if [ "$m" = "DP-4" ]; then
+	if [ "$m" = "DP-4" ] || [ "$m" = "eDP-1" ]; then
 		MONITOR=$m polybar -r laptop 2>>~/.config/polybar/.polybar.err &
 	else
 		MONITOR=$m polybar -r custom 2>>~/.config/polybar/.polybar.err &
