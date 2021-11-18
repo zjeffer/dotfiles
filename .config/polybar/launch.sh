6803aa12b042c3972fc4d74f59ea474e915f54fc
zjeffer <4633209+zjeffer@users.noreply.github.com>
2021-11-18 13:10:01 +0100

    feh now loads earlier when reloading, other optimizations

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,13 +3,6 @@
 # Terminate already running bar instances
 killall polybar 2>/dev/null
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null
-do 
-	pkill polybar 2>/dev/null
-	sleep 0.5
-done
-
 #Launch Polybar on every monitor, using default config location ~/.config/polybar/config
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	# put a different bar on the laptop monitor (because it has a higher resolution)
