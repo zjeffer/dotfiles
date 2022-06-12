b14db7102313996fce60d2b392e1594910fe9add
zjeffer <4633209+zjeffer@users.noreply.github.com>
2022-06-12 23:37:20 +0200

    feat(polybar): timestamp in logs

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,12 +3,15 @@
 # Terminate already running bar instances
 killall polybar 2>/dev/null
 
+log_file=$HOME/.config/polybar/.polybar.err
+
 #Launch Polybar on every monitor, using default config location ~/.config/polybar/config
 for m in $(polybar --list-monitors | cut -d":" -f1); do
 	# put a different bar on the laptop monitor (because it has a higher resolution)
-	if [ "$m" = "DP-4" ] || [ "$m" = "eDP-1" ]; then
-		MONITOR=$m polybar -r laptop 2>>~/.config/polybar/.polybar.err &
-	else
-		MONITOR=$m polybar -r custom 2>>~/.config/polybar/.polybar.err &
-	fi
+    if [ "$m" = "DP-4" ] || [ "$m" = "eDP-1" ]; then
+        bar_type="laptop"
+    else
+        bar_type="custom"
+	fi 
+    MONITOR=$m polybar -r $bar_type 2>&1 | ts "%Y-%m-%dT%H:%M:%S%z" >> "$log_file" &
 done
