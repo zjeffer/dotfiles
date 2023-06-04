e4b6701e3c4f603b963e11ff97d98dbc54e47b3b
zjeffer <4633209+zjeffer@users.noreply.github.com>
2023-06-04 11:47:00 +0200

    hyprland updates

--- .config/hypr/apps/waybar/launch.sh
+++ .config/hypr/apps/waybar/launch.sh
@@ -6,10 +6,12 @@ STYLE_PATH="$WAYBAR_FOLDER/style.css"
 # space-separated string of files to watch
 CONFIG_FILES="$CONFIG_PATH $STYLE_PATH"
 
-trap "killall waybar" EXIT
+# kill waybar when this script exits
+trap "pkill -f media-player.py -9 2>/dev/null && killall waybar 2>/dev/null" EXIT
 
 while true; do
     waybar --config $CONFIG_PATH --style $STYLE_PATH 2>$WAYBAR_FOLDER/.waybar.err &
     inotifywait -e create,modify $CONFIG_FILES
-    killall waybar
+    pkill -f media-player.py 2>/dev/null
+    killall waybar 2>/dev/null
 done
