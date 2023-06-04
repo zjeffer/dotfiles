c8a651eec96767740694d24545ca36e35e07ec41
zjeffer <4633209+zjeffer@users.noreply.github.com>
2023-06-04 11:42:39 +0200

    media-player.py fixes

--- .config/hypr/apps/waybar/scripts/media-player.py
+++ .config/hypr/apps/waybar/scripts/media-player.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+import gi
+gi.require_version("Playerctl", "2.0")
+from gi.repository import Playerctl, GLib
+from gi.repository.Playerctl import Player
 import argparse
 import logging
 import sys
@@ -6,8 +10,7 @@ import signal
 import gi
 import json
 import os
-gi.require_version("Playerctl", "2.0")
-from gi.repository import Playerctl, GLib
+from typing import List
 
 logger = logging.getLogger(__name__)
 
@@ -18,12 +21,15 @@ def signal_handler(sig, frame):
     # loop.quit()
     sys.exit(0)
 
+
 class PlayerManager:
     def __init__(self, selected_player=None):
         self.manager = Playerctl.PlayerManager()
         self.loop = GLib.MainLoop()
-        self.manager.connect("name-appeared", lambda *args: self.on_player_appeared(*args))
-        self.manager.connect("player-vanished", lambda *args: self.on_player_vanished(*args))
+        self.manager.connect(
+            "name-appeared", lambda *args: self.on_player_appeared(*args))
+        self.manager.connect(
+            "player-vanished", lambda *args: self.on_player_vanished(*args))
 
         signal.signal(signal.SIGINT, signal_handler)
         signal.signal(signal.SIGTERM, signal_handler)
@@ -44,24 +50,25 @@ class PlayerManager:
     def run(self):
         logger.info("Starting main loop")
         self.loop.run()
-    
+
     def init_player(self, player):
         logger.info(f"Initialize new player: {player.name}")
         player = Playerctl.Player.new_from_name(player)
-        player.connect("playback-status", self.on_playback_status_changed, None)
+        player.connect("playback-status",
+                       self.on_playback_status_changed, None)
         player.connect("metadata", self.on_metadata_changed, None)
         self.manager.manage_player(player)
         self.on_metadata_changed(player, player.props.metadata)
 
-    def get_players(self):
-        return self.manager.props.player_names
+    def get_players(self) -> List[Player]:
+        return self.manager.props.players
 
     def write_output(self, text, player):
         logger.debug(f"Writing output: {text}")
 
         output = {"text": text,
-                "class": "custom-" + player.props.player_name,
-                "alt": player.props.player_name}
+                  "class": "custom-" + player.props.player_name,
+                  "alt": player.props.player_name}
 
         sys.stdout.write(json.dumps(output) + "\n")
         sys.stdout.flush()
@@ -71,17 +78,42 @@ class PlayerManager:
         sys.stdout.flush()
 
     def on_playback_status_changed(self, player, status, _=None):
-        logger.debug(f"Received new playback status: {status}")
+        logger.debug(f"Playback status changed for player {player.props.player_name}: {status}")
         self.on_metadata_changed(player, player.props.metadata)
 
-    def on_metadata_changed(self, player, metadata, _=None):
-        logger.debug("Received new metadata")
-        track_info = ""
+    def get_first_playing_player(self):
+        players = self.get_players()
+        logger.debug(f"Getting first playing player from {len(players)} players")
+        if len(players) > 0:
+            # if any are playing, show the first one that is playing
+            # reverse order, so that the most recently added ones are preferred
+            for player in players[::-1]:
+                if player.props.status == "Playing":
+                    return player
+            # if none are playing, show the first one
+            return players[0]
+        else:
+            logger.debug("No players found")
+            return None
 
+    def show_most_important_player(self):
+        logger.info("\n\nShowing most important player")
+        # show the currently playing player
+        # or else show the first paused player
+        # or else show nothing
+        current_player = self.get_first_playing_player()
+        if current_player is not None:
+            self.on_metadata_changed(current_player, current_player.props.metadata)
+        else:    
+            self.clear_output()
+
+    def on_metadata_changed(self, player, metadata, _=None):
+        logger.debug(f"Metadata changed for player {player.props.player_name}")
         player_name = player.props.player_name
         artist = player.get_artist()
         title = player.get_title()
 
+        track_info = ""
         if player_name == "spotify" and "mpris:trackid" in metadata.keys() and ":ad:" in player.props.metadata["mpris:trackid"]:
             track_info = "Advertisement"
         elif artist is not None and title is not None:
@@ -94,30 +126,24 @@ class PlayerManager:
                 track_info = " " + track_info
             else:
                 track_info = " " + track_info
-        self.write_output(track_info, player)
+        # only print output if no other player is playing
+        current_playing = self.get_first_playing_player()
+        if current_playing is None or current_playing.props.player_name == player.props.player_name:
+            self.write_output(track_info, player)
+        else:
+            logger.debug(f"Other player {current_playing.props.player_name} is playing, skipping")
 
     def on_player_appeared(self, _, player):
+        logger.info(f"\n\nPlayer has appeared: {player.name}")
         if player is not None and (self.selected_player is None or player.name == self.selected_player):
             self.init_player(player)
         else:
-            logger.debug("New player appeared, but it's not the selected player, skipping")
+            logger.debug(
+                "New player appeared, but it's not the selected player, skipping")
 
     def on_player_vanished(self, _, player):
         logger.info("Player has vanished")
-        # check if other player is running
-        players = self.get_players()
-        if len(players) > 0:
-            # if any are playing, show the first one that is playing
-            for player in players[::-1]: # reverse order, so that the most recently added ones are preferred
-                if player.props.status == "Playing":
-                    self.on_metadata_changed(player, player.props.metadata)
-                    return
-                else:
-                    logger.debug(f"{player.props.player_name} is not playing, status = {player.props.status}")
-            # if none are playing, show the first one
-            self.on_metadata_changed(players[0], players[0].props.metadata)
-        else:
-            self.clear_output()
+        self.show_most_important_player()
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -132,13 +158,16 @@ def parse_arguments():
 
     return parser.parse_args()
 
+
 def main():
     arguments = parse_arguments()
 
     # Initialize logging
     if arguments.enable_logging:
-        logfile = os.path.join(os.path.dirname(os.path.realpath(__file__)), "media-player.log")
-        logging.basicConfig(filename=logfile, level=logging.DEBUG, format="%(asctime)s %(name)s %(levelname)s %(message)s")
+        logfile = os.path.join(os.path.dirname(
+            os.path.realpath(__file__)), "media-player.log")
+        logging.basicConfig(filename=logfile, level=logging.DEBUG,
+                            format="%(asctime)s %(name)s %(levelname)s:%(lineno)d %(message)s")
 
     # Logging is set by default to WARN and higher.
     # With every occurrence of -v it"s lowered by one
