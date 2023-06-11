89bfed6d1c6f46768c0c49b5d2acb66d1ce03be7
zjeffer <4633209+zjeffer@users.noreply.github.com>
2023-06-11 14:45:39 +0200

    updated nvim, added nord theme

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -12,9 +12,10 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "plugins" },
   },
   defaults = {
@@ -26,7 +27,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  -- install = { colorscheme = { "shaunsingh/nord" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
---
+++ .config/nvim/lua/plugins/nord.lua
@@ -0,0 +1,12 @@
+return {
+  -- add nord
+  { "shaunsingh/nord.nvim" },
+
+  -- Configure LazyVim to load nord
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
