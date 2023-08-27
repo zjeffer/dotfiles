f289c83c17ebf3e70b1b5ec12cdd5db07cf347c7
zjeffer <4633209+zjeffer@users.noreply.github.com>
2023-08-27 22:42:22 +0200

    nvim

--- .config/nvim/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- Disable relative numbering: always show exact line number
-vim.opt.relativenumber = false
--- Disable conceal: show "" in json files
-vim.opt.conceallevel = 0
