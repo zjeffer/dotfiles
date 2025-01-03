9c007b5943a61465614da1d541adbef5092cbca6
zjeffer <4633209+zjeffer@users.noreply.github.com>
2025-01-03 18:13:41 +0100

    zsh aliases

---
+++ .config/zsh/aliases.sh
@@ -0,0 +1,30 @@
+# File to save aliases and exports to clean up ~ folder
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# Aliases
+alias ls='ls --color=auto'
+alias la='ls -lah'
+alias l='ls -lAh'
+alias ll='ls -l'
+alias free='free -h'
+alias df='df -h'
+alias ka='killall'
+alias vim='nvim'
+alias nvim='nvim'
+alias feh='feh --auto-zoom'
+alias dmesg='sudo dmesg -H --color'
+alias gs='git status'
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
+alias yay='paru'
+alias less='less -I '
+alias yarn='yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config'
+alias discord='discord --no-sandbox'
+
+source $XDG_CONFIG_HOME/zsh/gh-copilot-alias.sh
+
+# for proper ssh access with kitty
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
+
