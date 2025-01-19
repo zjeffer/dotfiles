d0fbeccf9680ee68d0c591d59b12619310a4d494
zjeffer <4633209+zjeffer@users.noreply.github.com>
2025-01-19 23:31:52 +0100

    readd some git aliases that were previously part of oh-my-zsh

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -12,12 +12,17 @@ alias vim='nvim'
 alias nvim='nvim'
 alias feh='feh --auto-zoom'
 alias dmesg='sudo dmesg -H --color'
-alias gs='git status'
 alias wget='wget --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 alias yay='paru'
 alias less='less -I '
 alias yarn='yarn --use-yarnrc $XDG_CONFIG_HOME/yarn/config'
 alias discord='discord --no-sandbox'
+
+# git 
+alias gs='git status'
+alias gd='git diff'
+alias gl='git pull'
+alias g='git'
 
 source $XDG_CONFIG_HOME/zsh/gh-copilot-alias.sh
 
