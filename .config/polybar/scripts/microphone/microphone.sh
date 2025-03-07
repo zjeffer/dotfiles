4b4a34170d77bf46414a76b3f986f037c2616684
zjeffer <4633209+zjeffer@users.noreply.github.com>
2025-03-07 17:08:45 +0100

    cleanup of old config files from previous installations

--- .config/polybar/scripts/microphone/microphone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ $(pactl get-source-mute $(pactl get-default-source 2>/dev/null) 2>/dev/null | cut -d' ' -f2) == 'yes' ]; then 
-    echo ' '
-    #echo ''
-else
-    echo ''
-fi
