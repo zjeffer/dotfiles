0ca4b3e0db1818a1d7e168499a8036093a94c20d
zjeffer <4633209+zjeffer@users.noreply.github.com>
2025-07-25 23:55:38 +0200

    fix logout

--- .config/rofi/powermenu/type-4/powermenu.sh
+++ .config/rofi/powermenu/type-4/powermenu.sh
@@ -71,7 +71,7 @@ run_cmd() {
 			i3-msg exit
 		elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
 			qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-		elif [[ $XDG_CURRENT__DESKTOP == 'Hyprland' ]]; then
+		elif [[ $XDG_CURRENT_DESKTOP == 'Hyprland' ]]; then
 			hyprctl dispatch exit
 		fi
 	fi
